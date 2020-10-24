a271fd4bea6b4550d03e6d456b853fe292ab1e5f
Braden Little <blittle485@gmail.com>
2020-10-24 13:39:23 -0400

    Added comments to the 'thread' package

--- src/main/java/com/bradenlittle/hobbes/async/thread/AsyncSQLStatementConsumer.java
+++ src/main/java/com/bradenlittle/hobbes/async/thread/AsyncSQLStatementConsumer.java
@@ -3,14 +3,24 @@ package com.bradenlittle.hobbes.async.thread;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 
+/**
+ * This class is used to asynchronously execute SQL statements. It is different from AsyncSQLQueryCallable because it is
+ * always waiting for new SQLTasks to execute. A BlockingQueue is used to ensure that there is no collision when using
+ * the database.
+ * @author Madrugaur (https://github.com/Madrugaur)
+ */
 public class AsyncSQLStatementConsumer implements Runnable {
     private Connection connection;
     private BlockingQueue<SQLTask> tasks;
 
+    /**
+     * Constructor, inits local variables
+     * @param tasks a BlockingQueue used to hold SQLTask until they are ready to be executed
+     * @param connection connection to the SQL database
+     */
     public AsyncSQLStatementConsumer(BlockingQueue<SQLTask> tasks, Connection connection) {
         this.tasks = tasks;
         this.connection = connection;
--- src/main/java/com/bradenlittle/hobbes/async/thread/SQLTaskExecutor.java
+++ src/main/java/com/bradenlittle/hobbes/async/thread/SQLTaskExecutor.java
@@ -9,24 +9,36 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.*;
 
+/**
+ * This class handles the execution of all of the SQLTasks submitted by the bot
+ * @author Madrugaur (https://github.com/Madrugaur)
+ */
 public class SQLTaskExecutor {
-    private static BlockingQueue<SQLTask> tasks;
-    private static Connection connection;
-    private static Thread thread;
-    private static ExecutorService executorService;
+    private static final BlockingQueue<SQLTask> tasks;
+    private static final Connection connection;
+    private static final ExecutorService executorService;
 
     static {
         connection = IO.getSQLConnection(InformationBucket.fromPackage("sql.url").replace("{resource_dir}", IO.getResourceDir()));
         tasks = new LinkedBlockingQueue<>();
-        thread = new Thread(new AsyncSQLStatementConsumer(tasks, connection));
+        Thread thread = new Thread(new AsyncSQLStatementConsumer(tasks, connection));
         executorService = Executors.newFixedThreadPool(10);
         thread.start();
     }
 
+    /**
+     * Submits a SQLTask to the ExecutorService
+     * @param task the task to be executed
+     * @return the future representation of the results
+     */
     public static Future<ResultSet> query(SQLTask task) {
         return executorService.submit(new AsyncSQLQueryCallable(task, connection));
     }
 
+    /**
+     * Queues a task in the BlockingQueue
+     * @param task task to queue
+     */
     public static void queue(SQLTask task) {
         try {
             tasks.put(task);
@@ -35,6 +47,11 @@ public class SQLTaskExecutor {
         }
     }
 
+    /**
+     * Returns a list of SQL commands with their arguments embedded
+     * @param task task to format
+     * @return a list of formatted SQL commands
+     */
     public static List<String> packageStatements(SQLTask task) {
         List<String> statements = new LinkedList<String>();
         Iterator<String> commands = task.getStatements().iterator();
